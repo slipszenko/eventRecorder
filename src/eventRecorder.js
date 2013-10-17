25aa0956751858e68b41551b186ef67be2e826bb
Edward Slipszenko <edward.slipszenko@myport.ac.uk>
2013-10-17 15:22:22 +0100

    Need to find a way to save event objects. Normal JSON doesn't work.

--- src/eventRecorder.js
+++ src/eventRecorder.js
@@ -30,6 +30,17 @@ szko.eventRecorder = (function () {
 
     play = function() {
         console.log("Play back function called.");
+        // Create callback functions to run events that have happened
+        for(var i = 0; i < recordLog.length; i++) {
+            window.setTimeout(function(i) {
+                recordLog[i].event.target.dispatchEvent(recordLog[i].event);
+            }, recordLog[i].timeDelay);
+        }
+    },
+
+
+    setLog = function(log) {
+        recordLog = log;
     },
 
 
@@ -44,14 +55,13 @@ szko.eventRecorder = (function () {
                 'timeDelay' : (new Date().getTime()) - startTime,
                 'event' : e
             };
-            console.log("An action happened.", e);
         }
     },
 
 
     stopRecording = function() {
         // Remove event listeners
-        var everything = document.querySelectorAll("body, body *");
+        var everything = document.querySelectorAll("*");
         for(var i = 0; i < everything.length; i++) {
             for(var j = 0; j < allEventTypes.length; j++) {
                 everything[i].removeEventListener(allEventTypes[j], recordAction, false);
@@ -65,7 +75,7 @@ szko.eventRecorder = (function () {
 
     startRecording = function() {
         // Add event listeners
-        var everything = document.querySelectorAll("body, body *");
+        var everything = document.querySelectorAll("*");
         for(var i = 0; i < everything.length; i++) {
             for(var j = 0; j < allEventTypes.length; j++) {
                 everything[i].addEventListener(allEventTypes[j], recordAction, false);
@@ -79,7 +89,9 @@ szko.eventRecorder = (function () {
 
     // Return exposes some private functions as public
     return {
+        setLog : setLog.bind(szko.eventRecorder),
         getLog : getLog.bind(szko.eventRecorder),
+        play : play.bind(szko.eventRecorder),
         startRecording : startRecording.bind(szko.eventRecorder),
         stopRecording : stopRecording.bind(szko.eventRecorder)
     };
